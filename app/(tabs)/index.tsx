33a8ac6a89dca3095da32c6177999e4a6df6dec9
Vishal Kashi <dev.vishalkashi@gmail.com>
2023-12-09 00:56:51 +0530

    initial setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,92 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Button, StyleSheet } from "react-native";
+import axios from 'axios';
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+import * as Linking from "expo-linking";
+import { CONFIG } from "../../constants/Secret";
+import * as WebBrowser from "expo-web-browser";
+import { useEffect, useState } from "react";
+import URLParse from 'url-parse';
 
 export default function TabOneScreen() {
+  const [auth_state, setAuthState] = useState(null);
+  const [token, setToken] = useState(null);
+
+  useEffect(()=>{
+    if(auth_state){
+      const parsedUrl = new URLParse(auth_state?.event?.url, true);
+      const codeValue = parsedUrl.query.code;
+      getAuthToken(codeValue);
+    }
+  }, [auth_state])
+
+  const getAuthToken = async (code)=>{
+    console.log(code);
+    
+    try{
+      // const response = await axios.post('https://anilist.co/api/v2/oauth/token', {
+      //   'grant_type': 'authorization_code',
+      //   'client_id': CONFIG.CLIENT_ID,
+      //   'client_secret': CONFIG.SECRET,
+      //   'redirect_uri': CONFIG.REDIRECT_URI, // http://example.com/callback
+      //   'code': code, // The Authorization Code received previously
+      // }, { headers: {
+      //   'Content-Type': 'application/json',
+      //   'Accept': 'application/json',
+      // }});
+
+      const postData = {
+        grant_type: 'authorization_code',
+        client_id: CONFIG.CLIENT_ID,
+        client_secret: CONFIG.SECRET,
+        redirect_uri: "com.destructo570.dangoapprn://oauth", // http://example.com/callback
+        code: code, // The Authorization Code received previously
+      };
+      
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      };
+      
+      const response = await axios.post('https://anilist.co/api/v2/oauth/token', postData, config)
+  
+      if(response && response.status === 200){
+        console.log(response);
+        setToken(response);
+      }
+
+    }catch(err){
+      console.log(err); 
+    }
+
+
+  }
+  const onPressHandler = async () => {
+    _addLinkingListener();
+
+    let result = await WebBrowser.openBrowserAsync(CONFIG.AUTH_URL_2).then(
+      (result) => {
+        console.log("Here", result);
+      }
+    );
+    console.log(result);
+  };
+
+  const _addLinkingListener = () => {
+    Linking.addEventListener("url", _handleRedirect);
+  };
+
+  const _handleRedirect = (event) => {
+    setAuthState({ event });
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Button title="Login with Anilist" onPress={onPressHandler} />
+      {/* <Text style={{ marginTop: 30 }}>{JSON.stringify(auth_state)}</Text> */}
+      <Text style={{ marginTop: 30 }}>{JSON.stringify(token)}</Text>
     </View>
   );
 }
@@ -16,16 +94,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
---
+++ constants/Secret.ts
@@ -0,0 +1,10 @@
+export const CONFIG = {
+    CLIENT_ID: "15601",
+    SECRET: "YsPJCTBSCXARXoAj68LwKqdlB3ywURms7CQmlVmV",
+    REDIRECT_URI: "com.destructo570.dangoapprn",
+    REDIRECT_URI_SLUG: "com.destructo570.dangoapprn",
+    TOKEN_URL: "https://anilist.co/api/v2/oauth/token",
+    AUTH_ISSUER: "https://anilist.co/",
+    AUTH_URL: "https://anilist.co/api/v2/oauth/authorize",
+    AUTH_URL_2: "https://anilist.co/api/v2/oauth/authorize?client_id=15601&redirect_uri=com.destructo570.dangoapprn://oauth&response_type=code"
+}
