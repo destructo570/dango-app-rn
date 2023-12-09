e6a11ea07abe18a9c57a476b8eb4e5665b0e3b09
Vishal Kashi <dev.vishalkashi@gmail.com>
2023-12-09 23:29:37 +0530

    chore: update secret

--- constants/Secret.ts
+++ constants/Secret.ts
@@ -1,10 +1,7 @@
-export const CONFIG = {
+export const AUTH_CONFIG = {
     CLIENT_ID: "15601",
     SECRET: "YsPJCTBSCXARXoAj68LwKqdlB3ywURms7CQmlVmV",
-    REDIRECT_URI: "com.destructo570.dangoapprn",
-    REDIRECT_URI_SLUG: "com.destructo570.dangoapprn",
+    REDIRECT_URI: "com.destructo570.dangoapprn://oauth",
     TOKEN_URL: "https://anilist.co/api/v2/oauth/token",
-    AUTH_ISSUER: "https://anilist.co/",
-    AUTH_URL: "https://anilist.co/api/v2/oauth/authorize",
-    AUTH_URL_2: "https://anilist.co/api/v2/oauth/authorize?client_id=15601&redirect_uri=com.destructo570.dangoapprn://oauth&response_type=code"
+    AUTH_URL: "https://anilist.co/api/v2/oauth/authorize?client_id=15601&redirect_uri=com.destructo570.dangoapprn://oauth&response_type=code"
 }
